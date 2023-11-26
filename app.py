fc76aa13a960d1234d59c4378d62132b7c687d58
Chetan Sathish <98352066+CHEETAH02@users.noreply.github.com>
2023-11-26 19:55:04 +0530

    Add files via upload
    Second commit

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_pymongo import PyMongo
 from student import student
+from teacher import teacher
 
 app = Flask(__name__)
 app.register_blueprint(student, url_prefix="/student")
+app.register_blueprint(teacher, url_prefix="/teacher")
 
 client = PyMongo(app, uri="mongodb://localhost:27017/AutoExamPaperCorrection")
 db = client.db
@@ -13,48 +15,60 @@ app.secret_key = '192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bc
 def home():
     return render_template("index.html")
 
-@app.route("/TeacherLoginPage")
-def teacher_login_page():
-    return render_template("teacher_login.html")
+@app.route("/studentRegister", methods=['GET', 'POST'])
+def student_register():
+    if request.method == 'POST':
 
-@app.route("/StudentLoginPage")
-def student_login_page():
-    return render_template("student_login.html")
+        data = request.form
+        studentID = data['student_id']
+        studentName = data['student_name']
+        studentPassword = data['student_pwd']
 
-@app.route("/StudentRegisterPage")
-def student_registration_page():
-    return render_template("student_registration.html")
+        if db.students.find_one({"id": studentID}):
+            return render_template("student_exists.html")
 
-@app.route("/StudentRegistrationPage", methods=['POST'])
-def student_registration():
-    data = request.form
-    studentID = data['student_id']
-    studentName = data['student_name']
-    studentPassword = data['student_pwd']
+        dict = {
+            "id": studentID,
+            "name": studentName,
+            "password": studentPassword
+        }
 
-    if db.students.find_one({"id": studentID}):
-        return render_template("student_exists.html")
+        db.students.insert_one(dict)
+        return render_template("student_login.html")
+    
+    return render_template('student_register.html')
 
-    dict = {
-        "id": studentID,
-        "name": studentName,
-        "password": studentPassword
-    }
+@app.route("/studentLogin", methods=['GET', 'POST'])
+def student_login():
+    if request.method == 'POST':
 
-    db.students.insert_one(dict)
-    return render_template("student_login.html")
+        data = request.form
+        studentID = data['student_id']
+        studentPassword = data['student_pwd']
 
-@app.route("/studentLoginAuth", methods=['POST'])
-def student_login_authentication():
-    data = request.form
-    studentID = data['student_id']
-    studentPassword = data['student_pwd']
+        if db.students.find_one({"id": studentID, "password": studentPassword}):
+            session['logged_in'] = True
+            session['student'] = studentID
+            return redirect(url_for('student.student_home_page'))
+        return render_template('student_not_exists.html')
+    
+    return render_template('student_login.html')
 
-    if db.students.find_one({"id": studentID, "password": studentPassword}):
-        session['logged_in'] = True
-        session['student'] = studentID
-        return redirect(url_for('student.student_home_page'))
-    return render_template('student_not_exists.html')
+@app.route("/teacherLogin", methods=['GET', 'POST'])
+def teacher_login():
+    if request.method == 'POST':
+
+        data = request.form
+        teacherID = data['teacher_id']
+        teacherPassword = data['teacher_pwd']
+
+        if db.teachers.find_one({"id": teacherID, "password": teacherPassword}):
+            session['logged_in'] = True
+            session['teacher'] = teacherID
+            return redirect(url_for('teacher.teacher_home_page'))
+        return render_template('teacher_not_exists.html')
+    
+    return render_template('teacher_login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- teacher.py
+++ teacher.py
@@ -1,18 +1,18 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 from functools import wraps
 
-student = Blueprint('student', __name__)
+teacher = Blueprint('teacher', __name__)
 
-# To be put between every route decorator and function if student logged in
+# To be put between every route decorator and function if teacher logged in
 def logged_in(f):
     @wraps(f)
     def decorated_func(*args, **kwargs):
         if session.get("logged_in"):
             return f(*args, **kwargs)
-        return redirect(url_for("student_login"))
+        return redirect(url_for("teacher_login"))
     return decorated_func
 
-@student.route("/homePage")
+@teacher.route("/homePage")
 @logged_in
-def student_home_page():
-    return render_template('student_home_page.html')
+def teacher_home_page():
+    return render_template('teacher_home_page.html')
